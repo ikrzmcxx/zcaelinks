0971481a1443a9a6570c119a63b80a3bc2cca227
ikrzmcxx <99166161+ikrzmcxx@users.noreply.github.com>
2022-02-28 15:09:05 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+<head> <title> Performance Task In CAE </head> </title>
+<body style="background-color:purple;">
+
+<h1 style="font-size:300%;"> Performance Task in Communication Arts In English </h1>
+        <p>  "I regard the theater as the greatest of all art forms, the most immediate way in which a human being can share with another the sense of what it is to be a human being."
+           Oscar Wilde. </p>
+     <h1 style="font- size:150%;"> Cick Me :)) </h1>
+     <img src="cutie kitty.jpg   alt="caelink.com" width="104" height="142">
+</body>
+</html>
